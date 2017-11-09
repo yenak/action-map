a746e9092e5eee9c882bc9035da34246fa9b26a1
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-11-08 16:46:27 -0800

    TravisCI fix: Removed references to google api key until we can manage app API keys through encryption

--- config/initializers/ominauth.rb
+++ config/initializers/ominauth.rb
@@ -1,7 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   config = Settings.default["oauth2"]
-  provider :google_oauth2, config["client_id"],
-                          config["client_secret"],
-                          image_size: 150
-                          
+  # provider :google_oauth2, config["client_id"],
+  #                         config["client_secret"],
+  #                         image_size: 150
+  # UNCOMMENT THIS AND COMMENT THE BELOW LINES TO USE GOOGLE AUTH
+  # Workaround for tests until we can manage google api key
+  provider :google_oauth2, 0,
+                           0,
+                           image_size: 150
+
 end
