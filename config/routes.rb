7a139feba808da0118171d4e519883ccd49c2050
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-11-14 15:50:41 -0800

    Bugfix: Fixed routes to create person and created persons controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,9 @@ Rails.application.routes.draw do
   get "/auth/google_oauth2/callback", to: "sessions#create"
 
   get "/logout", to: "sessions#destroy"
-  get "/admin/person/create" to "persons#create", as: 'person_create'
-  get "/admin/person/:id" to "persons#edit", as: 'person_edit'
+  get "/admin/person/new" to "persons#new", as: 'new_person'
+  post "/admin/person" to "persons#create", as: 'create_person'
+  get "/admin/person/:id" to "persons#edit", as: 'edit_person'
 
   # get 'users/:id/interests' => 'users#interests', as: 'interests'
 
