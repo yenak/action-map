98618b0ec675cc286268e194ab7d5976ddba5e30
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-11-14 17:19:10 -0800

    Feature: persons can now be edited after being created and user will be directed to edit page after creating a new person

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -5,9 +5,22 @@ class PersonsController < ApplicationController
 		fullname = params[:fullname]
 		sex = params[:sex]
 		@person = Person.create!(name: fullname, birthday: birthdate, sex: sex)
-		redirect_to root_path
+		redirect_to edit_person_path(@person)
 	end
 
 	def new
+	end
+
+	def edit
+		@person = Person.find(params[:id])
+		new_name = params[:fullname] || @person.name
+		new_sex = params[:sex] || @person.sex
+		if params[:person]
+			string_date = params[:person].values.join("-")
+			new_birthday = Date.parse(string_date)
+		else
+			new_birthday = @person.birthday
+		end
+		@person.update_attributes(name: new_name, sex: new_sex, birthday: new_birthday)
 	end
 end
