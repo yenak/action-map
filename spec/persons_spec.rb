261e5f459401528a80cc12c7b72e597b65b6bf37
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-11-21 17:53:23 -0800

    Added test for sad path of PersonsController#show

--- spec/persons_spec.rb
+++ spec/persons_spec.rb
@@ -28,4 +28,16 @@ describe PersonsController, type: :controller do
             expect(response).to render_template(:show)
         end
     end
+
+    describe "Go to a non-existant person's page" do
+        it "tries to find the person" do
+            expect(Person).to receive(:find).with(999.to_s)
+            get 'show', {:id => 999}
+        end
+        it "tells the view that the person isn't found" do
+            get 'show', {:id => 999}
+            expect(assigns(:not_found)).to eq(true)
+        end
+
+    end
 end
