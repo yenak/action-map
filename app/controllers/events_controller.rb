a14eb3eb8fc8d8ff2c856a9e4ee98cf81869faa0
Yena Kim <yenakim@berkeley.edu>
2017-12-02 05:45:52 +0000

    refactored create event method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,44 +22,14 @@ class EventsController < ApplicationController
             redirect_to new_event_path
             return
         end
-        if event_params["start_time(1i)"] > event_params["end_time(1i)"]
-            # end year is before start year
+        @start_date = Time.new(event_params["start_time(1i)"].to_i, event_params["start_time(2i)"].to_i, event_params["start_time(3i)"], event_params["start_time(4i)"].to_i, event_params["start_time(5i)"].to_i)
+        @end_date = Time.new(event_params["end_time(1i)"].to_i, event_params["end_time(2i)"].to_i, event_params["end_time(3i)"], event_params["end_time(4i)"].to_i, event_params["end_time(5i)"].to_i)
+        puts @start_date
+        puts @end_date
+        if @start_date > @end_date
             flash[:date] = "Make sure end date is after start date."
             redirect_to new_event_path
             return
-        elsif event_params["start_time(1i)"] == event_params["end_time(1i)"]
-            # same start and end year
-            if event_params["start_time(2i)"] > event_params["end_time(2i)"]
-                # end month is before start month
-                flash[:date] = "Make sure end date is after start date."
-                redirect_to new_event_path
-                return
-            elsif event_params["start_time(2i)"] == event_params["end_time(2i)"]
-                # same start and end month
-                if event_params["start_time(3i)"] > event_params["end_time(3i)"]
-                    # end day is before start day
-                    flash[:date] = "Make sure end date is after start date."
-                    redirect_to new_event_path
-                    return
-                elsif event_params["start_time(3i)"] == event_params["end_time(3i)"]
-                    # same start and end day
-                    if event_params["start_time(4i)"] > event_params["end_time(4i)"]
-                        # end hour is before start hour
-                        flash[:date] = "Make sure end date is after start date."
-                        redirect_to new_event_path
-                        return
-                    elsif event_params["start_time(3i)"] == event_params["end_time(3i)"]
-                        # same start and end hour
-                        if event_params["start_time(5i)"] > event_params["end_time(5i)"]
-                            # end minute is before start minute
-                            flash[:date] = "Make sure end date is after start date."
-                            redirect_to new_event_path
-                            return
-                        end
-                    end
-                    
-                end
-            end
         end
         params.require(:state)
         @params = event_params.merge(:state => params[:state])
